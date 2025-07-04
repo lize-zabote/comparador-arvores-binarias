bece3a9830c560d3adf312759f021aa2ec4adcf0
icrcode <icarobotelhosocial@gmail.com>
2025-07-03 21:53:12 -0300

    update - change PASSO constant from 1 to 300 for improved sampling interval

--- experimento_arvores.c
+++ experimento_arvores.c
@@ -1047,7 +1047,7 @@ void rb_destruir(ArvoreRubro* arvore) {
 /* ============================================================================= */
 int main() {
     const int MAX_N = 10000;
-    const int PASSO = 1;
+    const int PASSO = 300;
     const int NUM_AMOSTRAS = 10;
     const int MAX_VALOR_CHAVE = 100000;
 
